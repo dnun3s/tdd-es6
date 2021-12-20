97c2b0fd06fe1c083a541c070f03265edccd1a3b
Luis Nunes <dnun3s@gmail.com>
2021-12-20 18:46:19 +0000

    [test pass]

--- src/letter-count.js
+++ src/letter-count.js
@@ -1,1 +1,12 @@
-export const getLetterCount = String => ({});
+export const getLetterCount = String => {
+    const letters = String.split('');
+    let letterCount = {};
+
+    letters.forEach(letter => {
+        if(!letterCount[letter]){
+            letterCount[letter] = 1;
+        }
+        
+    });
+    return letterCount
+};
